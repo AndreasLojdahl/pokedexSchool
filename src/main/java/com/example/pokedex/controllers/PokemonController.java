109c543aa257bd03aa1afb75b45ed83f875d3bd7
andreasLojdahl <andreas.lojdahl@gmail.com>
2020-10-29 13:00:32 +0100

    added crud mappings in pokemonController

--- src/main/java/com/example/pokedex/controllers/PokemonController.java
+++ src/main/java/com/example/pokedex/controllers/PokemonController.java
@@ -5,12 +5,12 @@ import com.example.pokedex.entities.Pokemon;
 import com.example.pokedex.services.PokemonConsumerService;
 import com.example.pokedex.services.PokemonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
@@ -35,4 +35,31 @@ public class PokemonController {
         var pokemons = pokemonService.findPokemons(params);
         return ResponseEntity.ok(pokemons);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pokemon> findPokemonById(@PathVariable String id){
+        return ResponseEntity.ok(pokemonService.findPokemonById(id));
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Pokemon> savePokemon(@RequestBody Pokemon pokemon){
+        var savePokemon = pokemonService.save(pokemon);
+        var uri = URI.create("/api/v1/pokemon/" + savePokemon.getId());
+        return ResponseEntity.created(uri).body(savePokemon);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updatePokemon(String id, Pokemon pokemon){
+        pokemonService.update(id, pokemon);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePokemon(@PathVariable String id){
+        pokemonService.delete(id);
+    }
+
+
 }
